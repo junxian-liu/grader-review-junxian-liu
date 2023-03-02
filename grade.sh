0fa2858adeed47a8a01a3d1ac55ba450229ba306
NumaanFormoli <numanformoliz@gmail.com>
2023-03-02 15:47:59 -0800

    Small changes

--- grade.sh
+++ grade.sh
@@ -1,5 +1,10 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
+//TODO: grade.sh
+
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+
+//Try writing the actual code
